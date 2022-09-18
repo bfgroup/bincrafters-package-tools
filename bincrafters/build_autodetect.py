6f17485fe95a2ed7bda06e26adec9ae3901cf73d
Rene Rivera <grafikrobot@gmail.com>
2022-09-17 21:37:10 -0500

    Allow sys package install in conan 2.

--- bincrafters/build_autodetect.py
+++ bincrafters/build_autodetect.py
@@ -106,7 +106,8 @@ def run_autodetect():
     os.system('conan config set general.revisions_enabled=1')
     os.environ["CONAN_DOCKER_ENTRY_SCRIPT"] =\
         "conan config set storage.download_cache='{}'; ".format(tmpdir)+\
-        "conan config set general.revisions_enabled=1 "
+        "conan config set general.revisions_enabled=1 "+\
+        "tools.system.package_manager:mode=install "
     conan_docker_run_options = os.environ.get('CONAN_DOCKER_RUN_OPTIONS','')
     conan_docker_run_options += " -v '{}':'/tmp/conan'".format(tmpdir)
     os.environ['CONAN_DOCKER_RUN_OPTIONS'] = conan_docker_run_options
