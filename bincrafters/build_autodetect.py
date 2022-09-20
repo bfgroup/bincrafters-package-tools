690df0190a342f360463a6039c5d1f71e5ad866a
Rene Rivera <grafikrobot@gmail.com>
2022-09-20 08:55:57 -0500

    Home is $HOME.

--- bincrafters/build_autodetect.py
+++ bincrafters/build_autodetect.py
@@ -112,7 +112,7 @@ conf.tools.system.package_manager:mode=install
     os.environ["CONAN_DOCKER_ENTRY_SCRIPT"] =\
         "conan config set storage.download_cache='{}' ; ".format(tmpdir)+\
         "conan config set general.revisions_enabled=1 ; "+\
-        "cp '{}/global.conf' ~/.conan/global.conf".format(tmpdir)
+        "cp '{}/global.conf' \\$HOME/.conan/global.conf".format(tmpdir)
     conan_docker_run_options = os.environ.get('CONAN_DOCKER_RUN_OPTIONS','')
     conan_docker_run_options += " -v '{}':'/tmp/conan'".format(tmpdir)
     os.environ['CONAN_DOCKER_RUN_OPTIONS'] = conan_docker_run_options
